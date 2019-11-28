e78cd40921459b2c6bbce4ad2f1ff885b7a19a19
Bradley Clarke <bradley.clarke@deltaware.com.au>
2019-11-28 15:15:19 +1100

    ADDED| RecyclingQueue and associated interface
    RecyclcingQueue will cycle through a specified sized queue size recycling old entries.

---
+++ src/deltaware.sdk.core/Collections/RecyclingQueue.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+
+using DeltaWare.SDK.Core.Interfaces;
+
+namespace DeltaWare.SDK.Core.Collections
+{
+    public class RecyclingQueue<T> : IRecyclingQueue<T>
+    {
+        private readonly T[] _queue;
+
+        private int _index;
+
+        public int Capacity => _queue.Length;
+
+        public T this[int index]
+        {
+            get => _queue[GetIndexOffset(index)];
+            set => _queue[GetIndexOffset(index)] = value;
+        } 
+
+        public RecyclingQueue(int capacity)
+        {
+            _queue = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            _queue[_index] = item;
+
+            _index++;
+
+            if (_index >= Capacity - 1)
+            {
+                _index = 0;
+            }
+        }
+
+        public void AddMany(T[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Add(items[i]);
+            }
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < _queue.Length; i++)
+            {
+                _queue[i] = default;
+            }
+
+            _index = 0;
+        }
+
+        public T[] GetRange(int index, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetIndexOffset(int index)
+        {
+            index += _index;
+
+            if (index > Capacity - 1)
+            {
+                index -= Capacity;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ src/deltaware.sdk.core/Interfaces/IRecyclingQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeltaWare.SDK.Core.Interfaces
+{
+    public interface IRecyclingQueue<T>
+    {
+        int Capacity { get; }
+
+        T this[int index] { get; set; }
+
+        void Add(T item);
+
+        void AddMany(T[] items);
+
+        void Clear();
+
+        T[] GetRange(int index, int count);
+    }
+}
