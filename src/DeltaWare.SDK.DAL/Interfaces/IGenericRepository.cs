3a4c1797db29e5b9d7b599275cd577eba3f495f5
Bradley Clarke <bradley.clarke@deltaware.com.au>
2020-01-19 23:29:50 +1100

    ADDED | Data Access SDK

---
+++ src/DeltaWare.SDK.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeltaWare.SDK.DAL.Interfaces
+{
+    public interface IGenericRepository<TRepo> where TRepo : class
+    {
+        Task<TRepo> GetByIdAsync(long id);
+
+        Task<TRepo> GetByIdentityAsync(Guid identity);
+
+        Task InsertAsync(TRepo siteModel);
+
+        void Update(TRepo siteModel);
+
+        Task DeleteAsync(long id);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
